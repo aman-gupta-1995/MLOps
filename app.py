33721a0fd5051490894c01fcc1fdfcc1ad42ec02
AMAN GUPTA <amangupta1995.ag@gmail.com>
2024-09-05 16:10:40 +0530

    Requirements file

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 import streamlit as st
 import yfinance as yf
 import datetime as dt
