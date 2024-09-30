98d2deb4c19d325223ede46015faa5bd6a61a1ec
AMAN GUPTA <amangupta1995.ag@gmail.com>
2024-10-01 01:00:07 +0530

    mlflow

---
+++ MLFlow/flow.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import mlflow 
